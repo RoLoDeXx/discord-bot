f4a873a6087809961e8bbdf50198f6b0bc963b1a
Sam <dexxrolo@gmail.com>
2024-02-27 00:45:19 +0530

    Add: basic skeleton and logging to the bot

---
+++ index.js
@@ -0,0 +1,21 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on("message", (message) => {
+  // if message.content
+  console.log(message);
+});
+
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+
+  if (interaction.commandName === "ping") {
+    await interaction.reply("Pong!");
+  }
+});
+
+client.login(TOKEN);
