abd057a80d92a9cc54a8eb15cbb229eeb0014b61
Sam <dexxrolo@gmail.com>
2024-02-27 01:25:40 +0530

    Add: basic hi message

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
+require("dotenv").config();
+
 const Discord = require("discord.js");
-const client = new Discord.Client();
+const GatewayIntentBits = Discord.GatewayIntentBits;
+const Client = Discord.Client;
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
+});
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on("message", (message) => {
-  // if message.content
+client.on("messageCreate", (message) => {
+  if (message.author.bot) return;
   console.log(message);
+  message.reply("hello! I'm under dev, don't expect much.");
 });
 
 client.on("interactionCreate", async (interaction) => {
@@ -18,4 +25,4 @@ client.on("interactionCreate", async (interaction) => {
   }
 });
 
-client.login(TOKEN);
+client.login(process.env.TOKEN);
